7a8e6b86bb35cc240d77c91127931ce7c816360e
Dantong Dong <dantong.dong@cloudera.com>
2021-06-09 16:15:11 -0700

    add dataconnector in thrift

--- standalone-metastore/metastore-common/src/gen/thrift/gen-cpp/hive_metastore_types.cpp
+++ standalone-metastore/metastore-common/src/gen/thrift/gen-cpp/hive_metastore_types.cpp
@@ -18,16 +18,18 @@ int _kHiveObjectTypeValues[] = {
   HiveObjectType::DATABASE,
   HiveObjectType::TABLE,
   HiveObjectType::PARTITION,
-  HiveObjectType::COLUMN
+  HiveObjectType::COLUMN,
+  HiveObjectType::DATACONNECTOR
 };
 const char* _kHiveObjectTypeNames[] = {
   "GLOBAL",
   "DATABASE",
   "TABLE",
   "PARTITION",
-  "COLUMN"
+  "COLUMN",
+  "DATACONNECTOR"
 };
-const std::map<int, const char*> _HiveObjectType_VALUES_TO_NAMES(::apache::thrift::TEnumIterator(5, _kHiveObjectTypeValues, _kHiveObjectTypeNames), ::apache::thrift::TEnumIterator(-1, NULL, NULL));
+const std::map<int, const char*> _HiveObjectType_VALUES_TO_NAMES(::apache::thrift::TEnumIterator(6, _kHiveObjectTypeValues, _kHiveObjectTypeNames), ::apache::thrift::TEnumIterator(-1, NULL, NULL));
 
 std::ostream& operator<<(std::ostream& out, const HiveObjectType::type& val) {
   std::map<int, const char*>::const_iterator it = _HiveObjectType_VALUES_TO_NAMES.find(val);
